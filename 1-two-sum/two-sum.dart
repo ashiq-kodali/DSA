9765c3cdc04a5932122cce84b5f2a16eb7ffc0e9
ASHIQ K <149581260+ashiq-kodali@users.noreply.github.com>
2025-09-30 17:51:32 +0530

    Time: 6 ms (77.62%) | Memory: 149.8 MB (50.00%) - LeetSync

--- 1-two-sum/two-sum.dart
+++ 1-two-sum/two-sum.dart
@@ -1,6 +1,6 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-     Map<int, int> numMap = {};
+     final Map<int, int> numMap = {};
      for (int i = 0; i < nums.length; i++) {
     int complement = target - nums[i];
     if (numMap.containsKey(complement)) {
