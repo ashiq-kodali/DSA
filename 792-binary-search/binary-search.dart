e898cbc184920ad613aaf4aa8b6d14e067701732
ASHIQ K <149581260+ashiq-kodali@users.noreply.github.com>
2025-11-17 00:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 157.5 MB (23.26%) - LeetSync

---
+++ 792-binary-search/binary-search.dart
@@ -0,0 +1,24 @@
+class Solution {
+  int search(List<int> nums, int target) {
+
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (left <= right) {
+
+      int mid = left + (right - left) ~/ 2;
+
+      int midValue = nums[mid];
+
+      if (midValue == target) {
+        return mid;
+      } else if (midValue < target) {
+        left = mid + 1;
+      } else { 
+        right = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+}
